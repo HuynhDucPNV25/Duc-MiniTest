5b1639d3e0ed9a8aa9ed2b16e3de659a1d886280
Huỳnh Đức <duc.huynh25@student.passerellesnumeriques.org>
2024-06-14 15:56:11 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+export const Header = ({pnLogo}) => {
+  return (
+    <>
+      <header id="header">
+        <img src={pnLogo} alt="PN Logo" />
+        <h1>Students results for (fake batch name))</h1>
+      </header>
+    </>
+  );
+};
